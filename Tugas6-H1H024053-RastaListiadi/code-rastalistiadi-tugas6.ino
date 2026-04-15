20434e7143aba8ae68e6c8231221a505fc75f687
razdient <rstazz06@gmail.com>
2026-04-15 22:20:36 +0700

    Update code-rastalistiadi-tugas6.ino

--- Tugas6-H1H024053-RastaListiadi/code-rastalistiadi-tugas6.ino
+++ Tugas6-H1H024053-RastaListiadi/code-rastalistiadi-tugas6.ino
@@ -1,1 +1,31 @@
+#include <Adafruit_LiquidCrystal.h>
+
+Adafruit_LiquidCrystal lcd(0);
+
+String teks = "7-0";
+
+void setup()
+{
+  lcd.begin(16, 2);
+  lcd.setBacklight(1);
+
+  lcd.setCursor(5, 0);
+  lcd.print("LFC MU");
+}
+
+void loop()
+{
+  for (int pos = 15; pos >= -3; pos--) 
+  {
+    lcd.setCursor(0, 1);
+    lcd.print("                "); 
+
+    if (pos >= 0) {
+      lcd.setCursor(pos, 1);
+      lcd.print(teks);
+    }
+
+    delay(100);
+  }
+}
 
